a38c424e3b696362f125ecf47c118f1f495f7be6
uambwani3 <ambwaniudayan01@gmail.com>
2023-01-16 18:45:22 -0800

    updated react -v

--- src/index.js
+++ src/index.js
@@ -1,18 +1,18 @@
 import React from "react";
-import ReactDOM from "react-dom/client";
-// import ReactDOM from 'react-dom';
+// import ReactDOM from "react-dom/client";
+import ReactDOM from "react-dom";
 import App from "./App";
 
-// ReactDOM.render(
-//   <React.StrictMode>
-//     <App />
-//   </React.StrictMode>,
-//   document.getElementById('root')
-// );
-
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(
+ReactDOM.render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>
+  </React.StrictMode>,
+  document.getElementById("root")
 );
+
+// const root = ReactDOM.createRoot(document.getElementById("root"));
+// root.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>
+// );
